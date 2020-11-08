da8423510d5066ec5722f318e0f8440c76b64202
Leah Riffell <leah.riffell@gmail.com>
2020-11-08 11:56:37 -0700

    Change name of time conversion method to datetime

--- app/poros/daily_forecast.rb
+++ app/poros/daily_forecast.rb
@@ -9,8 +9,8 @@ class DailyForecast
 
   def initialize(details)
     @date = convert_to_date(details[:dt])
-    @sunrise = convert_time(details[:sunrise])
-    @sunset = convert_time(details[:sunset])
+    @sunrise = convert_to_datetime(details[:sunrise])
+    @sunset = convert_to_datetime(details[:sunset])
     @max_temp = details[:temp][:max]
     @min_temp = details[:temp][:min]
     @conditions = details[:weather][0][:description]
@@ -21,7 +21,7 @@ class DailyForecast
     Time.at(unix_timestamp).strftime('%Y-%m-%d')
   end
 
-  def convert_time(unix_timestamp)
+  def convert_to_datetime(unix_timestamp)
     Time.at(unix_timestamp)
     # converts using local timezone
   end
