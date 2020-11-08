91fd6c729d71e29248ea2728ec2c8b8229b4c644
Leah Riffell <leah.riffell@gmail.com>
2020-11-08 12:03:52 -0700

    Add hourly forecast poro + spec

---
+++ app/poros/hourly_forecast.rb
@@ -0,0 +1,40 @@
+class HourlyForecast
+  attr_reader :time,
+              :wind_speed,
+              :wind_direction,
+              :conditions,
+              :icon
+
+  def initialize(details)
+    @time = convert_to_time(details[:dt])
+    @wind_speed = details[:wind_speed]
+    @wind_direction = convert_to_cardinal_directions(details[:wind_deg])
+    @conditions = details[:weather][0][:description]
+    @icon = details[:weather][0][:icon]
+  end
+
+  def convert_to_time(unix_timestamp)
+    Time.at(unix_timestamp).strftime('%H:%M:%S')
+  end
+
+  def convert_to_cardinal_directions(degrees)
+    case degrees
+    when 348.75..33.75
+      'N'
+    when 33.75..87.75
+      'NE'
+    when 78.75..123.75
+      'E'
+    when 123.75..168.75
+      'SE'
+    when 168.75..213.75
+      'S'
+    when 213.75..258.75
+      'SW'
+    when 258.75..303.75
+      'W'
+    when 303.75..348.75
+      'NW'
+    end
+  end
+end
---
+++ spec/poros/hourly_forecast_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe 'Hourly Forecast poro' do
+  it 'exists' do
+    attr = {
+            :dt=>1604872800,
+            :temp=>55.45,
+            :feels_like=>43.02,
+            :pressure=>1003,
+            :humidity=>20,
+            :dew_point=>16.95,
+            :clouds=>10,
+            :visibility=>10000,
+            :wind_speed=>12.46,
+            :wind_deg=>196,
+            :weather=>[{:id=>800, :main=>"Clear", :description=>"clear sky", :icon=>"01d"}],
+            :pop=>0
+          }
+          
+    forecast = HourlyForecast.new(attr)
+
+    expect(forecast).to be_a(HourlyForecast)
+
+    expect(forecast.time).to eq('15:00:00')
+    expect(forecast.wind_speed).to eq(attr[:wind_speed])
+    expect(forecast.wind_direction).to eq('S')
+    expect(forecast.conditions).to eq(attr[:weather][0][:description])
+    expect(forecast.icon).to eq(attr[:weather][0][:icon])
+  end
+end
