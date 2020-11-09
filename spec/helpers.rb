a5c7e95b270ae4602da5ebe93d36682f3a8bd727
Leah Riffell <leah.riffell@gmail.com>
2020-11-08 20:34:19 -0700

    Add test for image external img response structure

--- spec/helpers.rb
+++ spec/helpers.rb
@@ -116,6 +116,38 @@ module Helpers
     end
   end
 
+  def image_response_structure(response)
+    expect(response[:results]).to be_an(Array)
+    expect(response[:results].count).to eq(1)
+
+    first_result = response[:results][0]
+    expect(first_result).to be_a(Hash)
+
+    expect(first_result).to have_key(:description)
+    expect(first_result[:description]).to be_a(String)
+
+    expect(first_result).to have_key(:urls)
+    expect(first_result[:urls]).to be_a(Hash)
+
+    expect(first_result[:urls]).to have_key(:regular)
+    expect(first_result[:urls][:regular]).to be_a(String)
+
+    expect(first_result).to have_key(:user)
+    expect(first_result[:user]).to be_a(Hash)
+
+    expect(first_result[:user]).to have_key(:username)
+    expect(first_result[:user][:username]).to be_a(String)
+
+    expect(first_result[:user]).to have_key(:name)
+    expect(first_result[:user][:name]).to be_a(String)
+
+    expect(first_result[:user]).to have_key(:links)
+    expect(first_result[:user][:links]).to be_a(Hash)
+
+    expect(first_result[:user][:links]).to have_key(:html)
+    expect(first_result[:user][:links][:html]).to be_a(String)
+  end
+
   # ----- API exposure helper methods -----
 
   def forecast_exposure_structure(parsed)
