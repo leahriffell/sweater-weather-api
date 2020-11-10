3956132c9f8244a4b4c60f0509843aefe67b2c1b
Leah Riffell <leah.riffell@gmail.com>
2020-11-10 11:52:28 -0700

    Add destination coordinates to route endpoint return

--- app/services/map_service.rb
+++ app/services/map_service.rb
@@ -14,7 +14,8 @@ class MapService
       request.params['from'] = start_location
       request.params['to'] = end_location
     end
-    route_duration(parse(response)[:route][:formattedTime])
+    parsed = parse(response)
+    { duration: trip_duration(parsed[:route][:formattedTime]), end_lat_lng: end_coordinates(parsed) }
   end
 
   private
@@ -27,11 +28,19 @@ class MapService
     JSON.parse(response.body, symbolize_names: true)
   end
 
-  def self.route_duration(response)
+  def self.trip_duration(response)
     if response.nil?
       'impossible'
     else
       response
+    end
+  end
+
+  def self.end_coordinates(response)
+    if response[:route][:routeError]
+      nil
+    else
+      [:route][:locations][1][:latLng]
     end
   end
 end
--- spec/services/map_service_spec.rb
+++ spec/services/map_service_spec.rb
@@ -14,8 +14,19 @@ RSpec.describe MapService do
 
     it 'can fetch the trip duration for a road trip' do
       response = MapService.fetch_trip_duration('Denver, CO', 'Middlebury, IN')
-      expect(response).to be_a(String)
-      expect(response.include?(':'))
+      expect(response).to be_a(Hash)
+      expect(response).to have_key(:duration)
+      expect(response[:duration]).to be_a(String)
+      expect(response[:duration].include?(':'))
+    end
+
+    it 'can fetch the destination lat and long from route endpoint' do
+      response = MapService.fetch_trip_duration('Denver, CO', 'Middlebury, IN')
+      expect(response[:end_lat_lng]).to be_a(Hash)
+      expect(response[:end_lat_lng]).to have_key(:lat)
+      expect(response[:end_lat_lng]).to have_key(:lng)
+      expect(response[:end_lat_lng][:lat]).to be_a(Numeric)
+      expect(response[:end_lat_lng][:lng]).to be_a(Numeric)
     end
   end
 
@@ -32,7 +43,8 @@ RSpec.describe MapService do
     describe 'fetch_trip_duration' do
       it "returns 'impossible' if roadtrip is not possible" do
         response = MapService.fetch_trip_duration('New York, NY', 'Tokyo, Japan')
-        expect(response).to eq('impossible')
+        expect(response[:duration]).to eq('impossible')
+        expect(response[:end_lat_lng]).to eq(nil)
       end
 
       it 'no matching cities' do
