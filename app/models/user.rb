685e2494c6ac9b1c5a3707ea4a44a263eb3e1c68
Leah Riffell <leah.riffell@gmail.com>
2020-11-10 14:49:01 -0700

    Add road trip model and spec (belongs_to user)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   has_secure_token :api_key
+  has_many :road_trips
 
   validates :email, uniqueness: true, presence: true
   validates :password, presence: true
