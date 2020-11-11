5458d35a3e221ce78a6b782238c0baa07a8642c3
Leah Riffell <leah.riffell@gmail.com>
2020-11-10 19:41:36 -0700

    Add road trip facade spec

--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -1,10 +1,22 @@
 class RoadTripFacade
   def self.new_trip(origin, destination, user_key)
-    details = MapService.fetch_trip_duration(origin, destination)
-    duration = details[:duration]
-    weather = WeatherService.future_forecast(details[:end_lat_lng][:lat], details[:end_lat_lng][:lng], duration)
-    user = User.find_by(api_key: user_key)
-    trip = RoadTrip.create(start_city: origin, end_city: destination, travel_time: duration, user: user)
-    RoadTripDetails.new(trip, weather)
+    if MapService.fetch_trip_duration(origin, destination)[:duration] == 'impossible'
+      user = User.find_by(api_key: user_key)
+      trip = RoadTrip.create(start_city: origin, end_city: destination, travel_time: 'impossible', user: user)
+      RoadTripDetails.new({start_city: origin, end_city: destination, travel_time: 'impossible'}, '')
+    else
+      details = MapService.fetch_trip_duration(origin, destination)
+      user = User.find_by(api_key: user_key)
+      duration = details[:duration]
+      weather = WeatherService.future_forecast(details[:end_lat_lng][:lat], details[:end_lat_lng][:lng], duration)
+      trip = RoadTrip.create(start_city: origin, end_city: destination, travel_time: duration, user: user)
+      RoadTripDetails.new(trip, weather)
+    end
+  end
+
+  private
+
+  def self.impossible_trip(start_city, end_city)
+    RoadTripDetails.new({start_city: start_city, end_city: end_city, travel_time: 'impossible'}, '')
   end
 end
--- app/poros/road_trip_details.rb
+++ app/poros/road_trip_details.rb
@@ -8,6 +8,14 @@ class RoadTripDetails
     @start_city = trip[:start_city]
     @end_city = trip[:end_city]
     @travel_time = trip[:travel_time]
-    @weather_at_eta = { temperature: weather[:temp], conditions: weather[:weather][0][:description] }
+    @weather_at_eta = weather_at_arrival(weather)
+  end
+
+  def weather_at_arrival(weather)
+    if weather == ""
+      { temperature: '', conditions: '' }
+    else
+      { temperature: weather[:temp], conditions: weather[:weather][0][:description] }
+    end
   end
 end
