054364d9e2d2de613391da4f14aca7c3b9e0c801
Leah Riffell <leah.riffell@gmail.com>
2020-11-11 08:26:05 -0700

    Add image facade sad path test

--- spec/facades/image_facade_spec.rb
+++ spec/facades/image_facade_spec.rb
@@ -14,4 +14,21 @@ RSpec.describe 'Forecast Facade' do
       expect(image.credits[:profile_url]).to be_a(String)
     end
   end
+
+  it 'returns no results when no matching photos found' do
+    VCR.use_cassette('image_no_results') do
+      image = ImageFacade.get_background('tfyhjkn')
+
+      expect(image).to be_a(Hash)
+
+      expect(image).to have_key(:total)
+      expect(image[:total]).to eq(0)
+
+      expect(image).to have_key(:total_pages)
+      expect(image[:total_pages]).to eq(0)
+
+      expect(image).to have_key(:results)
+      expect(image[:results]).to eq([])
+    end
+  end
 end
